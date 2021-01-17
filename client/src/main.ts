33dc730fb0765f39514db69b44510ce1b0dc3aed
KanKK <286454796@qq.com>
2021-01-17 16:30:42 +0800

    feat(client): 添加 Element-Plus

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
 import App from './App.vue';
+import 'element-plus/lib/theme-chalk/index.css';
 
 import router from './routers';
 import store from './store';
@@ -12,6 +14,9 @@ app.use(router);
 
 // 注册 vuex
 app.use(store);
+
+// 注册 Element-Plus
+app.use(ElementPlus);
 
 // 渲染模板
 app.mount('#app');
